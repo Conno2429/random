5a9322b96d2641d3d0ebe4ff6631f8be1d2f7256
Connor Chen <connozebeast@gmail.com>
2018-08-28 20:12:39 -0700

    my 'own' generator

--- random.js
+++ random.js
@@ -1,3 +1,18 @@
-var random = Math.floor(Math.random() * 100);
+var random1 = Math.floor(Math.random() * 100);
 
-console.log('Random Number Generated:' + random);
+//console.log('Random Number Generated:' + random);
+
+var time = new Date();
+var curTime = time.getTime();
+
+
+function random2(seed) {
+    var seed = curTime;
+    var val = seed / (10000000000000);
+    return (val);
+}
+
+var rannum2 = random2(curTime);
+console.log("Random #1", random1);
+console.log("Random #2:", rannum2);
+
