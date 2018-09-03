041a37b960eb65e4fdd412459326b7990cbaa258
Connor Chen <connozebeast@gmail.com>
2018-09-03 15:37:53 -0700

    generate multiple random numbers

--- random.js
+++ random.js
@@ -2,17 +2,38 @@ var random1 = Math.floor(Math.random() * 100);
 
 //console.log('Random Number Generated:' + random);
 
-var time = new Date();
-var curTime = time.getTime();
+var denom = 101;
 
 
-function random2(seed) {
-    var seed = curTime;
-    var val = seed / (10000000000000);
+
+
+function random2() {
+    var seed = Date.now();
+    var val = Math.pow(seed % denom, 5);
+    denom++;
     return (val);
 }
 
-var rannum2 = random2(curTime);
+ function genRandomNumbers(count) {
+    var result = [];
+    for (var i = 1; i <= count; i++) {
+        result.push(random2());
+    }
+    return result;
+}
+
+function printList(list) {
+    for (i = 0; i < list.length; i++) {
+        console.log(list[i]);
+    }
+}
+
 console.log("Random #1", random1);
-console.log("Random #2:", rannum2);
+console.log("Random #2:", (random2()));
+
+var rlist = genRandomNumbers(2000);
+// console.log(rlist);
+printList(rlist);
+
+
 
